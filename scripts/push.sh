8d3d7ae48e3de4b324aa07b11dfee81534ffa405
TheMysteriousVincent <agpheaddikilla@yahoo.de>
2020-04-22 11:11:00 +0200

    update .travis.yml

---
+++ scripts/push.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker build . --build-arg BRANCH=$BRANCH -t $IMAGE:$BUILD
+docker tag $IMAGE:$BUILD $IMAGE:$BRANCH
+
+docker build . -f ./dotnet.Dockerfile --build-arg BUILD=$BUILD --build-arg BRANCH=$BRANCH -t $IMAGE:$BUILD-dotnet
+docker tag $IMAGE:$BUILD-dotnet $IMAGE:$BRANCH-dotnet
+
+docker build . -f ./nodejs.Dockerfile --build-arg BUILD=$BUILD --build-arg BRANCH=$BRANCH -t $IMAGE:$BUILD-nodejs
+docker tag $IMAGE:$BUILD-nodejs $IMAGE:$BRANCH-nodejs
+
+docker build . -f ./nodejs-dotnet.Dockerfile --build-arg BUILD=$BUILD-dotnet --build-arg BRANCH=$BRANCH -t $IMAGE:$BUILD-nodejs-dotnet
+docker tag $IMAGE:$BUILD-nodejs-dotnet $IMAGE:$BRANCH-nodejs-dotnet
+
+docker push $IMAGE:$BUILD
+docker push $IMAGE:$BRANCH
+
+docker push $IMAGE:$BRANCH-dotnet
+docker push $IMAGE:$BUILD-dotnet
+
+docker push $IMAGE:$BUILD-nodejs
+docker push $IMAGE:$BRANCH-nodejs
+
+docker push $IMAGE:$BUILD-nodejs-dotnet
+docker push $IMAGE:$BRANCH-nodejs-dotnet
