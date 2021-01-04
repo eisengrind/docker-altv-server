44063245b28dd8b7f01d4a97ff17d5328b712449
Vincent Heins <agpheaddikilla@yahoo.de>
2021-01-04 16:37:32 +0100

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,10 +4,15 @@ IMAGE=$1
 BRANCH=$2
 BUILD=$3
 
+HASH=$(echo "$BRANCH-$BUILD-$(date)" | sha1sum)
+SHORT_HASH=$($HASH | cut -c1-8)
+
 echo "build image"
 docker build . --build-arg BRANCH=$BRANCH -t $IMAGE:$BUILD
 docker tag $IMAGE:$BUILD $IMAGE:$BRANCH
 docker tag $IMAGE:$BUILD $IMAGE:$BRANCH-$BUILD
+docker tag $IMAGE:$BUILD $IMAGE:$HASH
+docker tag $IMAGE:$BUILD $IMAGE:$BRANCH-$BUILD-$SHORT_HASH
 
 # push it!
 
@@ -15,3 +20,5 @@ echo "push base image"
 docker push $IMAGE:$BUILD
 docker push $IMAGE:$BRANCH
 docker push $IMAGE:$BRANCH-$BUILD
+docker push $IMAGE:$HASH
+docker push $IMAGE:$BRANCH-$BUILD-$SHORT_HASH
