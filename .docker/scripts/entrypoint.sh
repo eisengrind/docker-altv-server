b4c237880437021d22bd02d66927b070ca31aff5
pixlcrashr <agpheaddikilla@yahoo.de>
2022-12-21 00:08:10 +0100

    fix: change default config filename

--- .docker/scripts/entrypoint.sh
+++ .docker/scripts/entrypoint.sh
@@ -3,5 +3,5 @@
 ALTV_SERVER_LOG_PATH=${ALTV_SERVER_LOG_PATH:-""}
 ALTV_SERVER_NO_LOGFILE=${ALTV_SERVER_NO_LOGFILE:-"true"}
 
-/opt/altv/configurator create --output server.cfg
-./altv-server --config=/opt/altv/server.cfg $ALTV_SERVER_LOG_PATH $ALTV_SERVER_NO_LOGFILE ${@:1}
+/opt/altv/configurator create --output server.toml
+./altv-server --config=/opt/altv/server.toml $ALTV_SERVER_LOG_PATH $ALTV_SERVER_NO_LOGFILE ${@:1}
